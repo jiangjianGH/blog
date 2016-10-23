ea0609222302783de681db47179491eecba64dd7
Jiangjian (Jack Jiang <j00160659@china.huawei.com>
2016-10-23 20:20:35 +0800

    Committer: Jiangjian (Jack Jiang <j00160659@china.huawei.com>  On branch master 	modified:   .gitignore 	new file:   .project 	new file:   blog-api/.classpath 	new file:   blog-api/.gitignore 	new file:   blog-api/.project 	new file:   blog-api/.swagger-codegen-ignore 	new file:   blog-api/.travis.yml 	new file:   blog-api/pom.xml 	new file:   blog-api/src/main/java/.gitignore 	new file:   blog-api/src/main/java/.swagger-codegen-ignore 	new file:   blog-api/src/main/java/.travis.yml 	new file:   blog-api/src/main/resources/blog.yaml 	new file:   blog-api/src/main/resources/uber.yaml 	new file:   blog-persist/.classpath 	new file:   blog-persist/.project 	new file:   blog-persist/pom.xml 	new file:   blog-persist/src/main/java/org/blog/presist/model/BlogPO.java 	new file:   blog-service/.classpath 	new file:   blog-service/.project 	new file:   blog-service/pom.xml 	new file:   blog-service/src/main/java/org/blog/service/App.java 	new file:   blog-service/src/test/java/org/blog/service/AppTest.java 	new file:   blog-web/.classpath 	new file:   blog-web/.project 	new file:   blog-web/pom.xml 	new file:   blog-web/src/main/java/Test.java 	new file:   blog-web/src/main/resources/jdbc.properties 	new file:   blog-web/src/main/resources/log4j.properties 	new file:   blog-web/src/main/resources/spring/spring-mybatis.xml 	new file:   blog-web/src/main/webapp/WEB-INF/web.xml 	new file:   blog-web/src/main/webapp/index.jsp 	new file:   pom.xml

---
+++ blog-persist/src/main/java/org/blog/presist/model/BlogPO.java
@@ -0,0 +1,25 @@
+package org.blog.presist.model;
+
+public class BlogPO {
+	Integer id;
+	String title;
+	String content;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
